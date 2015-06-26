745b0b9dc41f2e66b2b7b0e67a8adc599c51e808
AYTAGH <ayhamghraowi@gmail.com>
2015-06-26 09:53:41 -0400

    creates md file and structures the data to standard label template

--- label-book.py
+++ label-book.py
@@ -4,66 +4,101 @@ from sys import argv
 import json
 import cooperhewitt.api.client
 import os
-
-ACCESS_TOKEN = os.getenv('CH_ACCESS_TOKEN')
+import urllib
+import secrets
 
 if __name__ == "__main__":
 	
-	api = cooperhewitt.api.client.OAuth2(ACCESS_TOKEN)
+	api = cooperhewitt.api.client.OAuth2(secrets.ACCESS_TOKEN)
 
-	args = {'exhibition_id': '68744913', 'has_images': '1', 'page':'1', 'per_page':'100'}
+	args = {'exhibition_id': '68744915', 'has_images': '1', 'page':'1', 'per_page':'100'}
 
 	rsp = api.execute_method('cooperhewitt.exhibitions.getObjects', args)
 
 	target = open('book.md', 'w')
 
 	for item in rsp['objects']:
+
+		obj_id = item['id']
 		
+		images = item['images']
+		if images:
+			print obj_id
+			print images[0]
+			
+			img = images[0]['n']['url']	
+			target.write("![Object ID: "+obj_id+"]("+img+")")
+		target.write("\n")			
+														
+		# First Section-includes: Title, Date
 		title =	item['title'].encode('utf-8')
-		target.write("# Title: " + title)
+		target.write("# " + title)
 		target.write("\n")
 		
 		if (item['date']):
 			date = item['date'].encode('utf-8')
-			target.write("## Date: " + date)
-			target.write("\n")
-		
+			target.write("## " + date)
+		target.write("\n")
+
+		target.write("___")	
+		target.write("\n")
+
+		# Second Section-includes: Designer, Manufacturer, Material, Donor Info.
+		if (item['participants']):
+			for item2 in item['participants']:
+				role_name = item2['role_name'].encode('utf-8')
+				if role_name == 'Designer':
+
+					target.write("+ Designed by " + item2['person_name'].encode('utf-8'))
+					target.write("\n")
+
+		if (item['participants']):
+			for item2 in item['participants']:
+				role_name = item2['role_name'].encode('utf-8')
+				if role_name == 'Manufacturer':
+
+					target.write("+ Manufactured by " + item2['person_name'].encode('utf-8'))
+					target.write("\n")
+
 		if (item['medium']):
 			medium = item['medium'].encode('utf-8')
-			target.write("+ Medium: " + medium)
+			target.write("+ " + medium)
 			target.write("\n")
 
+		if (item['creditline']):
+			creditline = item['creditline'].encode('utf-8')
+			target.write("+ " + creditline)
+		target.write("\n")		
+		
+		target.write("___")	
+		target.write("\n")
+		
 		# retrieving tags through another api	
 		obj_id = item['id']
 		
 		args = {'object_id': obj_id, 'page':'1', 'per_page':'100'}
 		rsp2 = api.execute_method('cooperhewitt.objects.tags.getTags', args)
-
-		target.write ("# ID: " + obj_id)
-		target.write("\n")
-		
-		target.write("+ Tags: ")
+		# Third Section-includes: List of tags, Label Text
 		
 		first = True
-		
 		for tag in rsp2['tags']:
 			tag_name = tag['name']
-			
-			if first:
-				first = False
-				target.write(tag_name)
-			else:
-				target.write(", " + tag_name)
-
+			if tag_name != None:
+				
+				if first:
+					first = False
+					target.write("+ ")
+					target.write(tag_name)
+				else:
+					target.write(", " + tag_name)
 		target.write("\n")
 
-
-		if (item['gallery_text']):
-			gallery_text = item['gallery_text'].encode('utf-8')
-			target.write("+ Info: " + gallery_text)
+		if (item['label_text']):
+			label_text = item['label_text'].encode('utf-8')
+			target.write("+ " + label_text)
 			target.write("\n")
 		
-		
+		target.write("___")	
 		target.write("\n")
 		target.write("\n")
 	
