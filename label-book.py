be1599576cc818cbbbb69c8f59fa1d596ecb6b63
Micah Walter <micahwalter@outlook.com>
2015-07-02 00:32:33 +0000

    making this a flask app

--- label-book.py
+++ label-book.py
@@ -6,26 +6,33 @@ import cooperhewitt.api.client
 import os
 import urllib
 
-ACCESS_TOKEN = os.environ['CH_API_KEY']
+CH_ACCESS_TOKEN = os.environ['CH_ACCESS_TOKEN']
 
-if __name__ == "__main__":
-	
-	api = cooperhewitt.api.client.OAuth2(ACCESS_TOKEN)
+## adding flask here ( MJW/20150701 )
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return build_page()
+
+def build_page():
+
+	api = cooperhewitt.api.client.OAuth2(CH_ACCESS_TOKEN)
 
 	args = {'exhibition_id': '68744915', 'has_images': '1', 'page':'1', 'per_page':'100'}
 	rsp = api.execute_method('cooperhewitt.exhibitions.getObjects', args)
 
-	target = open('book.html', 'w')
+	html = ""
+	html += "<html>"
+	html += "<head>"
+	html += "<link rel=\"stylesheet\" type=\"text/css\" href=\"static/theme.css\">" 
+	html += "</head>"
+	html += "<body>"
 
-	target.write("<html>") 
-	target.write("<head>")
-	target.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"theme.css\">")  
-	target.write("</head>") 
-	target.write("<body>")
 
-
-# 	INTRO PAGE		
-# 	retrieving exhibition info through another api	w/ exhibition id		
+	# 	INTRO PAGE		
+	# 	retrieving exhibition info through another api	w/ exhibition id		
 	args = {'exhibition_id': '68744915'}
 	rsp3 = api.execute_method('cooperhewitt.exhibitions.getInfo', args)
 	
@@ -33,138 +40,131 @@ if __name__ == "__main__":
 	extitle = exhibition['title'].encode('utf-8')
 	extext = exhibition['text'].encode('utf-8')
 	
-	target.write("<div class=\"object intro\">") 
-	target.write("<h1>" + extitle + "</h1>")
-	target.write("<p>" + extext + "</p>")			
-	target.write("</div>")
+	html += "<div class=\"object intro\">" 
+	html += "<h1>" + extitle + "</h1>"
+	html += "<p>" + extext + "</p>"			
+	html += "</div>"
 				
-# 	end INTRO PAGE
-
-
+	# 	end INTRO PAGE
 
 	for item in rsp['objects']:
-		target.write("<div class=\"object\">") 
+		html += "<div class=\"object\">" 
 
-# 		store object ID in 'obj_id'		
+		# store object ID in 'obj_id'		
 		obj_id = item['id']
 
-
-
-# 		find images[]		
+		# find images[]		
 		images = item['images']
 
-# 		if statement to find index value of [0] in images[] and exclude images[] that are null		
-		target.write("<div class=\"col-1-3\">")
+		# if statement to find index value of [0] in images[] and exclude images[] that are null		
+		html += "<div class=\"col-1-3\">"
 		if images:
 			img = images[0]['n']['url']	
-			target.write("<img src=\"" + img + "\">")
+			img = "<img src=\"" + img + "\">"
+			html += img.encode('utf-8')
 		else: 
-			target.write("<h1>NO IMAGE</h1>")
-		target.write("</div>")
-
-
-		target.write("<div class=\"col-2-3\">")
-# 		sect. I														
-		target.write("<div class=\"section\">")
+			html += "<h1>NO IMAGE</h1>"
 		
-# 		sect. I - title
+		html += "</div>"
+
+		html += "<div class=\"col-2-3\">"
+		
+		# sect. I														
+		html += "<div class=\"section\">"
+		
+		# sect. I - title
 		if item['title_raw']:
 			title_raw =	item['title_raw'].encode('utf-8')
-# 			print obj_id
-# 			print title_raw
-			target.write("<h1>" + title_raw + "</h1>")
+			# print obj_id
+			# print title_raw
+			html += "<h1>" + title_raw + "</h1>"
 		else:	
 			title =	item['title'].encode('utf-8')
-# 			print obj_id
-# 			print title	
-			target.write("<h1>" + title + "</h1>")
+			# print obj_id
+			# print title	
+			html += "<h1>" + title + "</h1>"
 
-# 		sect. I - date		
+		# sect. I - date		
 		if (item['date']):
 			date = item['date'].encode('utf-8')
-			target.write("<h2>" + date + "</h2>")
+			html += "<h2>" + date + "</h2>"
 
-		target.write("</div>")
-# 		end sect. I														
-	
-	
-	
-	
-			
+		html += "</div>"
+		# end sect. I														
+		
+		# sect. II
+		html += "<div class=\"section\">"
+		html += "<ul class=\"info\">"													
 
-# 		sect. II
-		target.write("<div class=\"section\">")
-		target.write("<ul class=\"info\">")													
-
-# 		sect. II - particpants[] role_name: designer
+		# sect. II - particpants[] role_name: designer
 		if (item['participants']):
 			for item2 in item['participants']:
 				role_name = item2['role_name'].encode('utf-8')
 				if role_name == 'Designer':
-					target.write("<li>Designed by " + item2['person_name'].encode('utf-8') + "</li>")
+					html += "<li>Designed by " + item2['person_name'].encode('utf-8') + "</li>"
 					
-# 		sect. II - particpants[] role_name: manufacturer
+		# sect. II - particpants[] role_name: manufacturer
 		if (item['participants']):
 			for item2 in item['participants']:
 				role_name = item2['role_name'].encode('utf-8')
 				if role_name == 'Manufacturer':
-					target.write("<li>Manufactured by " + item2['person_name'].encode('utf-8') + "</li>")				
+					html += "<li>Manufactured by " + item2['person_name'].encode('utf-8') + "</li>"				
 
-# 		sect. II - medium
+		# sect. II - medium
 		if (item['medium']):
 			medium = item['medium'].encode('utf-8')
-			target.write("<li>" + medium + "</li>")
+			html += "<li>" + medium + "</li>"
 
-# 		sect. II - creditline
+		# sect. II - creditline
 		if (item['creditline']):
 			creditline = item['creditline'].encode('utf-8')
-			target.write("<li>" + creditline + "</li>")
+			html += "<li>" + creditline + "</li>"
 								
-		target.write("</ul>")
-		target.write("</div>")
-# 		end sect. II
+		html += "</ul>"
+		html += "</div>"
+		# end sect. II
 
-
-
-# 		sect. III
+		# sect. III
 															
-# 		sect. III - tags		
-# 		retrieving tags through another api	w/ obj id		
+		# sect. III - tags		
+		# retrieving tags through another api	w/ obj id		
 		args = {'object_id': obj_id, 'page':'1', 'per_page':'100'}
 		rsp2 = api.execute_method('cooperhewitt.objects.tags.getTags', args)
 				
 		tags = rsp2['tags']
 		
 		if tags:
-			target.write("<div class=\"section\">")
-			target.write("<ul class=\"tags\">")													
+			html += "<div class=\"section\">"
+			html += "<ul class=\"tags\">"													
 			
 			for tag in rsp2['tags']:
 				tag_name = tag['name']
 				
 				if tag_name != None:
-					target.write("<li>" + tag_name + "</li>")
+					html += "<li>" + tag_name.encode('utf-8') + "</li>"
 	
-			target.write("</ul>")
-			target.write("</div>")
+			html += "</ul>"
+			html += "</div>"
 				
-# 		end sect. III
+			# end sect. III
 
-
-
-# 		sect. IV
+			# sect. IV
 													
-# 		sect. IV - label text
+		# sect. IV - label text
 		if (item['label_text']) != None:
-			target.write("<div class=\"section\">")
-			target.write("<p>")
+			html += "<div class=\"section\">"
+			html += "<p>"
 			
 			label_text = item['label_text'].encode('utf-8')
-			target.write(label_text + "</p>")
+			html += label_text + "</p>"
 
-			target.write("</div>")
-# 		end sect. IV
+			html += "</div>"
+		# end sect. IV
 
-		target.write("</div>") #col-2-3 div
-		target.write("</div>") #object div 	
-	target.close()
+		html += "</div>" #col-2-3 div
+		html += "</div>" #object div 	
+	return html
+	
+if __name__ == "__main__":
+	### note: this is setup for a local config. need to add proper settings for heroku ( MJW/20150701 )
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
