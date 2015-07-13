150bd4ba6cb9bb8c4caf077ae8e08ef18a2b6136
AYTAGH <ayhamghraowi@gmail.com>
2015-07-13 14:28:52 -0400

    Sorted exhibition, added location under image thumbnail

--- label-book.py
+++ label-book.py
@@ -18,7 +18,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	print request.url_root
 	api = cooperhewitt.api.client.OAuth2(CH_ACCESS_TOKEN)
 
 	## retreiving data from api to get list of exhibitions ( ATG/20150705 )
@@ -35,26 +34,31 @@ def exhibitiion(ex_id):
 	return build_page(ex_id)
 
 def build_page(ex_id):
-
 	api = cooperhewitt.api.client.OAuth2(CH_ACCESS_TOKEN)
-
-	args = {'exhibition_id': ex_id, 'has_images': '1', 'page':'1', 'per_page':'100'}
-	rsp = api.execute_method('cooperhewitt.exhibitions.getObjects', args)
 
 	# 	retrieving exhibition info through another api	w/ exhibition id		
 	args = {'exhibition_id': ex_id}
 	rsp2 = api.execute_method('cooperhewitt.exhibitions.getInfo', args)
 
+	
+	args = {'exhibition_id': ex_id, 'has_images': '1', 'page':'1', 'per_page':'100'}
+	rsp = api.execute_method('cooperhewitt.exhibitions.getObjects', args)
+
 	data = rsp['objects']
 	for item in data:
 		
 		obj_id = item['id']
+
 		args = {'object_id': obj_id, 'page':'1', 'per_page':'100'}
-		tags = api.execute_method('cooperhewitt.objects.tags.getTags', args)
+		tagsarray = api.execute_method('cooperhewitt.objects.tags.getTags', args)
+
+		item['tags'] = tagsarray['tags']
+
+		args = {'object_id': obj_id, 'page':'1', 'per_page':'100'}
+		locationarray = api.execute_method('cooperhewitt.objects.isOnDisplay', args)
 		
-		item['tags'] = tags['tags']
+		item['locationinfo'] = locationarray
 				
-	
 	return render_template('obj_page.html',
 							data=data,
 							rsp2=rsp2)
