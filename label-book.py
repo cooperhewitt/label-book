18a5362ec93ed2ee72ecbc6280df8825503e3d6b
AYTAGH <ayhamghraowi@gmail.com>
2015-07-21 10:50:59 -0400

    trying out multiclient

--- label-book.py
+++ label-book.py
@@ -2,10 +2,13 @@
 
 from sys import argv
 import json
-import cooperhewitt.api.client
+#import cooperhewitt.api.client
 import os
 import urllib
+import pprint
 
+# For parallel processing via aaron
+import cooperhewitt.api.multiclient 
 
 CH_ACCESS_TOKEN = os.environ['CH_ACCESS_TOKEN']
 
@@ -13,51 +16,61 @@ CH_ACCESS_TOKEN = os.environ['CH_ACCESS_TOKEN']
 from flask import Flask, request
 from flask import render_template
 app = Flask(__name__)
-
 #
+
+
 
 @app.route("/")
 def home():
-	api = cooperhewitt.api.client.OAuth2(CH_ACCESS_TOKEN)
-
-	## retreiving data from api to get list of exhibitions ( ATG/20150705 )
+	api = cooperhewitt.api.multiclient.OAuth2(CH_ACCESS_TOKEN)
 	args = {'page':'1', 'per_page':'100'}
 	rsp4 = api.execute_method('cooperhewitt.exhibitions.getList', args)
 
 	return render_template('index.html',
 							rsp4=rsp4)
 
-#
+
+
 
 @app.route("/<int:ex_id>")
 def exhibitiion(ex_id):
 	return build_page(ex_id)
 
 def build_page(ex_id):
-	api = cooperhewitt.api.client.OAuth2(CH_ACCESS_TOKEN)
+	api = cooperhewitt.api.multiclient.OAuth2(CH_ACCESS_TOKEN)
+
+	reqs = (
+		('cooperhewitt.exhibitions.getInfo', {'exhibition_id': ex_id}),
+		('cooperhewitt.exhibitions.getObjects', {'on_display': '1', 'exhibition_id': ex_id, 'has_images': '1', 'page':'1', 'per_page':'100'}),	
+		#('cooperhewitt.objects.tags.getTags', {'object_id': obj_id, 'page':'1', 'per_page':'100'}),
+		#('cooperhewitt.objects.isOnDisplay', {'object_id': obj_id, 'page':'1', 'per_page':'100'}),
+	)
 
 	# 	retrieving exhibition info through another api	w/ exhibition id		
-	args = {'exhibition_id': ex_id}
-	rsp2 = api.execute_method('cooperhewitt.exhibitions.getInfo', args)
+	rsp2 = api.execute_method(*reqs[0])
+	print pprint.pformat(rsp2)
+ 
 
-	
-	args = {'on_display': '1', 'exhibition_id': ex_id, 'has_images': '1', 'page':'1', 'per_page':'100'}
-	rsp = api.execute_method('cooperhewitt.exhibitions.getObjects', args)
+	# 	retrieving images for respective exhibition			
+	rsp = api.execute_method(*reqs[1])
+	print pprint.pformat(rsp)
 
 	data = rsp['objects']
 	for item in data:
-		
 		obj_id = item['id']
 
-		args = {'object_id': obj_id, 'page':'1', 'per_page':'100'}
-		tagsarray = api.execute_method('cooperhewitt.objects.tags.getTags', args)
-
-		item['tags'] = tagsarray['tags']
-
-		args = {'object_id': obj_id, 'page':'1', 'per_page':'100'}
-		locationarray = api.execute_method('cooperhewitt.objects.isOnDisplay', args)
+		reqs2 = (
+			('cooperhewitt.objects.tags.getTags', {'object_id': obj_id, 'page':'1', 'per_page':'100'}),
+			('cooperhewitt.objects.isOnDisplay', {'object_id': obj_id, 'page':'1', 'per_page':'100'}),	
+		)
 		
+		tagsarray = api.execute_method(*reqs2[0])
+		item['tags'] = tagsarray['tags']
+		print pprint.pformat(tagsarray)
+
+		locationarray = api.execute_method(*reqs2[1])
 		item['locationinfo'] = locationarray
+		print pprint.pformat(locationarray)
 				
 	return render_template('obj_page.html',
 							data=data,
